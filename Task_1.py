b73462500678b0f19d94a5f58fe9d0152e434e10
Himanshu <himanshusharama.work@gmail.com>
2026-01-02 18:30:51 +0530

    Add files via upload
    My first Assignment_1 solution

---
+++ Task_1.py
@@ -0,0 +1,22 @@
+# 1.  Takes two numbers as input from the user.
+# 2.  Performs the basic mathematical operations on these two numbers:
+# o	Addition
+# o	Subtraction
+# o	Multiplication
+# o	Division
+
+a = float(input("Enter 1st number: "))
+b = float(input("Enter 2nd number: "))
+c = a + b
+print('sum is : ',round(c))
+
+
+d = a - b
+print('subtraction is : ',round(d))
+
+e = a * b
+print('multiplication is : ',round(e))
+
+f = a / b
+print('division is : ',round(f))
+
